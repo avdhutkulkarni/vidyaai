fab9e14be2a11b0d9ec7045d78a7bd938265272d
avdhutkulkarni <avdhutm34@gmail.com>
2026-03-22 14:45:21 +0530

    fixed iframe

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,25 @@
 'use client'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useRouter } from 'next/navigation'
 import { onAuthStateChanged } from 'firebase/auth'
 import { auth } from '@/lib/firebase'
 
 export default function VidyaAI() {
   const router = useRouter()
+  const [checking, setChecking] = useState(true)
 
   useEffect(() => {
     const unsub = onAuthStateChanged(auth, (user) => {
       if (!user) {
         router.push('/')
+      } else {
+        setChecking(false)
       }
     })
     return () => unsub()
   }, [router])
+
+  if (checking) return null
 
   return (
     <iframe
